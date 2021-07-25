795be650b9f7579167e195119c434e9f9cc3d213
saqib-jallal <saqib.jallal@thoughtworks.com>
2021-07-25 13:55:45 +0530

    [ADD] Landing page layout and it's styles

--- client/src/components/layouts/Landing.js
+++ client/src/components/layouts/Landing.js
@@ -1,5 +1,21 @@
 import React from "react";
+import "./Landing.css";
 
-export default function Landing() {
-  return <div>Landing</div>;
-}
+const Landing = () => {
+  return (
+    <section id="landing">
+      <div className="container">
+        <div className="row center-xs center-sm center-md center-lg middle-xs middle-sm middle-md middle-lg">
+          <div className="showcase col-xs-10 col-sm-10 col-md-10 col-lg-10">
+            <h1>
+              Welcome to <span className="title">Biblioteca</span>
+            </h1>
+            <p>The Bangalore Public Library</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;

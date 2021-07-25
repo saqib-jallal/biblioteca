4bf0cf097a9a99ed9e41b7da112c89c3bdf5a901
saqib-jallal <saqib.jallal@thoughtworks.com>
2021-07-25 12:29:29 +0530

    [ADD] Navbar layout and it's styles

--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -1,10 +1,51 @@
+import "./Navbar.css";
 import React from "react";
-import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
 
-function Navbar(props) {
-  return <div>Navbar</div>;
-}
-
-Navbar.propTypes = {};
+const Navbar = () => {
+  const authLinks = (
+    <ul>
+      <li>
+        <NavLink exact activeClassName="active" to="/books">
+          Books
+        </NavLink>
+      </li>
+      <li>
+        <NavLink exact activeClassName="active" to="/signin">
+          SignOut
+        </NavLink>
+      </li>
+    </ul>
+  );
+  const guestLinks = (
+    <ul>
+      <li>
+        <NavLink exact activeClassName="active" to="/signin">
+          SignIn
+        </NavLink>
+      </li>
+      <li>
+        <NavLink exact activeClassName="active" to="/signup">
+          SignUp
+        </NavLink>
+      </li>
+    </ul>
+  );
+  return (
+    <header id="header">
+      <div className="container">
+        <div className="row center-xs end-sm end-md end-lg end-lg middle-xs middle-sm middle-md middle-lg">
+          <NavLink to="/" className="col-xs-12 col-sm-4 col-md-2 col-lg-2">
+            <h1 className="title">Biblioteca</h1>
+          </NavLink>
+          <nav id="navbar" className="col-xs-12 col-sm-8 col-md-10 col-lg-10">
+            {/* {!loading && (isLoggedIn ? authLinks : guestLinks)} */}
+            {guestLinks}
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Navbar;

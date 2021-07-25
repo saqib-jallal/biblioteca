ec0859c9ef6a309a11fdbad6616e8d2d422d5e7d
saqib-jallal <saqib.jallal@thoughtworks.com>
2021-07-25 11:30:39 +0530

    [ADD] Basic component structure for react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
-import React, { Fragment } from "react";
+import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layouts/Navbar";
+import Landing from "./components/layouts/Landing";
+import Alert from "./components/layouts/Alert";
 
 const App = () => {
-  return <Fragment>Biblioteca</Fragment>;
+  return (
+    <Router>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <Alert />
+      <Switch>
+        <Route exact path="#" />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
